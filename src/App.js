78e0936100a2df7b2faa7056b2ff838f3f63411d
fdrive422 <fafrago@gmail.com>
2022-02-11 19:34:02 -0800

    update app.js formats

--- src/App.js
+++ src/App.js
@@ -3,39 +3,40 @@ import "./App.css";
 import PersonCard from "./components/PersonCard";
 
 function App() {
-  return (
+    return (
 
-    <div className="App">
+        <div className="App">
 
-      <PersonCard
-        last_name="Doe"
-        first_name="Jane"
-        age={45}
-        hair_color="Black"
-      />
+            <PersonCard
+                last_name="Doe"
+                first_name="Jane"
+                age={45}
+                hair_color="Black"
+            />
 
-      <PersonCard
-        last_name="Smith"
-        first_name="John"
-        age={88}
-        hair_color="Brown"
-      />
+            <PersonCard
+                last_name="Smith"
+                first_name="John"
+                age={88}
+                hair_color="Brown"
+            />
 
-      <PersonCard
-        last_name="Fillmore"
-        first_name="Millard"
-        age={50}
-        hair_color="Brown"
-      />
+            <PersonCard
+                last_name="Fillmore"
+                first_name="Millard"
+                age={50}
+                hair_color="Brown"
+            />
 
-      <PersonCard
-        last_name="Smith"
-        first_name="Maria"
-        age={62}
-        hair_color="Brown"
-      />
-    </div>
-  );
+            <PersonCard
+                last_name="Smith"
+                first_name="Maria"
+                age={62}
+                hair_color="Brown"
+            />
+
+        </div>
+    );
 }
 
 export default App;
